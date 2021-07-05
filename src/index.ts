20bed1659cf188fd104f7cb5851e47173e2503ed
greinisch <greinisch@youwol.com>
2021-07-05 17:37:06 +0200

    :sparkles: materialo module

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
  * Public API Surface of flux-arch
  */
 export * from './lib/main';
+export * from './lib/material.module'
 export * from './lib/boundary-condition.module'
 export * from './lib/constraint-coulomb.module'
 export * from './lib/constraint-coulomb-ortho.module'
