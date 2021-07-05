4e972ddd81d8e847df9505e4638a501b70d1d353
greinisch <greinisch@youwol.com>
2021-07-05 17:38:32 +0200

    :zap: main=> load arche.js @ install

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -1,8 +1,29 @@
 import { FluxPack, IEnvironment } from '@youwol/flux-core'
+import { Observable } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 import { AUTO_GENERATED } from '../auto_generated'
 
+export var arche 
+
+
 export function install(environment: IEnvironment){
-    return
+    
+    let loadWasm = environment.fetchJavascriptAddOn(`${AUTO_GENERATED.name}#${AUTO_GENERATED.version}~assets/arche.js`)
+    .pipe(
+        mergeMap( () => {
+            return new Observable(subscriber => {
+                window["ArcheModule"]().then( archeMdle => { 
+                    arche = archeMdle; 
+                    subscriber.next(true); subscriber.complete(); })
+            });
+        })
+        )
+
+    /*return forkJoin( [
+        Backend.fetchRenderViewCss( {href:`/api/cdn-backend/libraries/youwol/${NAME}/${VERSION}/assets/style.css`}),
+        loadWasm
+    ])*/
+    return loadWasm
 }
 
 export let pack = new FluxPack({
