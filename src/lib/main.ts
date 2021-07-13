f06af4d490c565f85bf624b24375fff929545a14
greinisch <greinisch@youwol.com>
2021-07-13 09:28:06 +0200

    :zap: main.ts => worker-pool setup

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -1,38 +1,42 @@
 import { FluxPack, IEnvironment } from '@youwol/flux-core'
 import { Observable } from 'rxjs';
-import { mergeMap } from 'rxjs/operators';
+import { mergeMap, tap } from 'rxjs/operators';
 import { AUTO_GENERATED } from '../auto_generated'
-import { WorkerPool } from './worker-pool';
-
+import { ArcheFacade } from './arche.facades';
 export var arche 
 
 
 export function install(environment: IEnvironment){
     
+    let resource = `${AUTO_GENERATED.name}#${AUTO_GENERATED.version}~assets/arche.js`
     let loadWasm = environment.fetchJavascriptAddOn(`${AUTO_GENERATED.name}#${AUTO_GENERATED.version}~assets/arche.js`)
     .pipe(
-        mergeMap( (assets) => {
-            let src = assets[0].src
-            WorkerPool.import({
-                sources: { 
-                    "@youwol/arche": {
-                        src: src,
-                        sideEffects: (workerScope, exports) => {
-                            return exports.ArcheModule().then( (arche) => { 
-                                console.log("WASM Arche installed", arche)
-                                workerScope.arche = arche
-                            })
-                        }
+        tap( (assets) => {
+            environment.workerPool.import({
+                sources: [{
+                    id: resource,
+                    src: assets[0].src,
+                    sideEffects: (workerScope, exports) => {
+                        return exports.ArcheModule().then( (arche) => { 
+                            console.log("WASM Arche installed", arche)
+                            workerScope.arche = arche
+                        })
                     }
-                }
+                }],
+                functions: [{
+                    id: "@youwol/flux-arche.archeFactory",
+                    target: ArcheFacade.factory
+                }]
             })
+        }),
+        mergeMap( (assets) => {
             return new Observable(subscriber => {
                 window["ArcheModule"]().then( archeMdle => { 
                     arche = archeMdle; 
                     subscriber.next(true); subscriber.complete(); })
             });
         })
-        )
+    )
 
     /*return forkJoin( [
         Backend.fetchRenderViewCss( {href:`/api/cdn-backend/libraries/youwol/${NAME}/${VERSION}/assets/style.css`}),
